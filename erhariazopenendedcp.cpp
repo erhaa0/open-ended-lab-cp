d6167371c0af3b1e13aaa3a74e41a4da74d49368
erha riaz <146338364+erhaa0@users.noreply.github.com>
2023-11-02 16:30:08 +0500

    Add files via upload

---
+++ erhariazopenendedcp.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	bool ItIsPrime = true;
+	int i, n;
+	int option, number, start, end;
+	string number = "prime";
+
+	for (i = 2; i * i <= n; i += 1)
+	{
+		if (n % i == 0) {
+			false;
+		}
+	}
+	return
+
+		true;
+	cout << "find primse number from a range (1) or give us a specific number (2)" << endl;
+	cin >> option;
+
+
+	switch (option) {
+
+	}
+
+
+
+
+	return 0;
